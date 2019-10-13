f50a2b34e458d923c51c300512d88f71ef8fe04f
zahra memar <zahra.memar72@gmail.com>
2019-10-13 08:58:55 +0200

    make some new component

---
+++ src/LeftMessage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LeftMessage() {
+  return <div>Rmessages</div>;
+}
---
+++ src/RightMessage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RightMessage() {
+  return <div>left</div>;
+}
